66f3d12e0f130fcc917c5af764243ff33a4266a0
droidnitk <droidnitk@gmail.com>
2017-03-12 14:41:13 +0530

    help page left

--- app/src/main/java/com/example/android/rapid54/MainActivity.java
+++ app/src/main/java/com/example/android/rapid54/MainActivity.java
@@ -7,13 +7,19 @@ import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Build;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.lang.String;
+import java.lang.Object;
 
 
 
@@ -28,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPref;
     SharedPreferences.Editor editor;
 
+    public void help(View view){
+        if (timeo != null){
+            timeo.cancel();
+        }
+        setContentView(R.layout.activity_help);
+    }
 
 
 
@@ -41,129 +53,136 @@ public class MainActivity extends AppCompatActivity {
 
         boolean first = sharedPref.getBoolean("first", true);
 
+        if (first){
+            help(findViewById(R.id.helpbutton));
+            editor.putBoolean("first", false);
+            editor.apply();
+        }
+
+        else {
+
+            List<Integer> list = new ArrayList<>();
+
+            int num, count = 1;
+
+            for (int i = 1; i <= 9; i++) {
+
+                int id = getResources().getIdentifier("LL" + i, "id", getPackageName());
+                LinearLayout LL = (LinearLayout) findViewById(id);
+                LL.setGravity(Gravity.CENTER);
+
+                int x = LL.getWidth() / 6, y = ((LL.getHeight() - 24) / 2);
+
+                for (int j = 0; j < 6; j++) {
 
 
-        List<Integer> list = new ArrayList<>();
+                    int resid = getResources().getIdentifier("text" + count, "id", getPackageName());
+                    text = (TextView) findViewById(resid);
+                    count++;
 
-        int num, count = 1;
+                    while (true) {
 
-        for (int i = 1; i<=9; i++){
-
-            int id = getResources().getIdentifier("LL" + i, "id", getPackageName());
-            LinearLayout LL = (LinearLayout) findViewById(id);
-            LL.setGravity(Gravity.CENTER);
-
-            int x = LL.getWidth()/6, y = ((LL.getHeight() - 24)/2);
-
-            for (int j = 0; j<6; j++) {
-
-
-
-                int resid = getResources().getIdentifier("text"+count, "id", getPackageName());
-                text = (TextView) findViewById(resid);
-                count++;
-
-                while (true) {
-
-                    Random ran = new Random();
-                    num = ran.nextInt(54) + 1;
-                    if (!list.contains(num)) {
-                        list.add(num);
-                        break;
+                        Random ran = new Random();
+                        num = ran.nextInt(54) + 1;
+                        if (!list.contains(num)) {
+                            list.add(num);
+                            break;
+                        }
                     }
-                }
 
-                text.setPadding(2, -y, 2, -y);
-                text.setWidth(x);
-                text.setText(""+num);
-                text.setTextSize(24);
-                text.setId(num);
-                text.setBackgroundColor(Color.parseColor("#FFFFFF"));
-                text.setClickable(true);
+                    text.setPadding(2, -y, 2, -y);
+                    text.setWidth(x);
+                    text.setText("" + num);
+                    text.setTextSize(24);
+                    text.setId(num);
+                    text.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                    text.setClickable(true);
 
-                text.setOnClickListener(new View.OnClickListener() {
+                    text.setOnClickListener(new View.OnClickListener() {
 
-                    @Override
-                    public void onClick(View v) {
-                        int a = v.getId();
-                        if (a == c){
-                            c++;
-                            TextView now = (TextView) findViewById(v.getId());
-                            now.setBackgroundColor(Color.parseColor("#424242"));
-                            now.setText(" ");
-                            now.setClickable(false);
+                        @Override
+                        public void onClick(View v) {
+                            int a = v.getId();
+                            if (a == c) {
+                                c++;
+                                TextView now = (TextView) findViewById(v.getId());
+                                now.setBackgroundColor(Color.parseColor("#424242"));
+                                now.setText(" ");
+                                now.setClickable(false);
 
-                            if (c==55){
-                                mintime = sharedPref.getInt("mintime", 60);
-                                if (mintime > time){
-                                    mintime = time;
-                                    editor.putInt("mintime", mintime);
-                                    editor.apply();
+                                if (c == 55) {
+                                    mintime = sharedPref.getInt("mintime", 60);
+                                    if (mintime > time) {
+                                        mintime = time;
+                                        editor.putInt("mintime", mintime);
+                                        editor.apply();
+                                    }
+                                    if (timeo != null) {
+                                        timeo.cancel();
+                                    }
+                                    setContentView(R.layout.activity_2);
+                                    TextView disp = (TextView) findViewById(R.id.disp);
+                                    disp.setText("CONGRATS!");
+                                    TextView yscore = (TextView) findViewById(R.id.yourscore);
+                                    yscore.setText("Your Score : " + time + " sec");
+                                    yscore.setTextSize(20);
+                                    TextView mscore = (TextView) findViewById(R.id.maxscore);
+                                    mscore.setText("Max Score : " + mintime + " sec");
+                                    mscore.setTextSize(20);
                                 }
-                                if (timeo != null){
+                            } else {
+                                TextView now = (TextView) findViewById(v.getId());
+                                now.setBackgroundColor(Color.parseColor("#D50000"));
+
+                                if (timeo != null) {
                                     timeo.cancel();
                                 }
-                                setContentView(R.layout.activity_2);
-                                TextView disp = (TextView) findViewById(R.id.disp);
-                                disp.setText("CONGRATS!");
-                                TextView yscore = (TextView) findViewById(R.id.yourscore);
-                                yscore.setText("Your Score : " + time + " sec");
-                                yscore.setTextSize(20);
-                                TextView mscore = (TextView) findViewById(R.id.maxscore);
-                                mscore.setText("Max Score : " + mintime + " sec");
-                                mscore.setTextSize(20);
+                                now.setClickable(false);
+
+
+                                Runnable r = new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        setContentView(R.layout.activity_2);
+
+                                        c = c - 1;
+                                        maxscore = sharedPref.getInt("maxscore", 0);
+                                        if (maxscore < c) {
+                                            maxscore = c;
+                                            editor.putInt("maxscore", maxscore);
+                                            editor.apply();
+                                        }
+                                        TextView disp = (TextView) findViewById(R.id.disp);
+                                        disp.setText("GAME OVER");
+                                        TextView yscore = (TextView) findViewById(R.id.yourscore);
+                                        yscore.setText("Your Score : " + c);
+                                        yscore.setTextSize(20);
+                                        TextView mscore = (TextView) findViewById(R.id.maxscore);
+                                        mscore.setText("Max Score : " + maxscore);
+                                        mscore.setTextSize(20);
+                                    }
+                                };
+
+                                Handler h = new Handler();
+                                h.postDelayed(r, 1000);
+
+
                             }
                         }
-                        else{
-                            TextView now = (TextView) findViewById(v.getId());
-                            now.setBackgroundColor(Color.parseColor("#D50000"));
-
-                            if (timeo != null){
-                                timeo.cancel();}
-                            now.setClickable(false);
+                    });
 
 
-
-
-
-                            setContentView(R.layout.activity_2);
-
-                            c = c-1;
-                            maxscore = sharedPref.getInt("maxscore", 0);
-                            if (maxscore < c){
-                                maxscore = c;
-                                editor.putInt("maxscore", maxscore);
-                                editor.apply();
-                            }
-                            TextView disp = (TextView) findViewById(R.id.disp);
-                            disp.setText("GAME OVER");
-                            TextView yscore = (TextView) findViewById(R.id.yourscore);
-                            yscore.setText("Your Score : " + c);
-                            yscore.setTextSize(20);
-                            TextView mscore = (TextView) findViewById(R.id.maxscore);
-                            mscore.setText("Max Score : " + maxscore);
-                            mscore.setTextSize(20);
-
-
-
-
-
-                        }
-                    }
-                });
-
+                }
 
 
             }
-
-
-
-
-
+            start_timer();
         }
-        start_timer();
 
+    }
 
+    public void makeToast(){
+        Toast.makeText(this, "Time Up", Toast.LENGTH_LONG).show();
     }
 
     public void playAgain(View view){
@@ -179,11 +198,16 @@ public class MainActivity extends AppCompatActivity {
         public void onTick(long millisUntilFinished) {
             timer.setText("" + millisUntilFinished / 1000);
             time = 60 - (int) millisUntilFinished/1000;
+            if (time >= 50){
+                timer.setTextColor(Color.parseColor("#D50000"));
+            }
 
         }
 
         @Override
         public void onFinish() {
+
+            makeToast();
 
             setContentView(R.layout.activity_2);
             c = c-1;
---
+++ app/src/main/java/com/example/android/rapid54/MainPageActivity.java
@@ -0,0 +1,29 @@
+package com.example.android.rapid54;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        TextView name = (TextView) findViewById(R.id.name);
+        Typeface face = Typeface.createFromAsset(getAssets(),
+                "fonts/rugsnatcher.ttf");
+        name.setTypeface(face);
+
+    }
+
+    public void play(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
